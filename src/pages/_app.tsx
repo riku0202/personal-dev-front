e9862fe5f0ed8da30a230c61ed7a6fe330a7e550
riku0202 <riku0202excalibur@gmail.com>
2022-01-06 08:12:36 +0900

    修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,59 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import type { ReactElement, ReactNode } from "react";
 import React from "react";
-import type { NextPage } from "next";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import "../styles/globals.css";
 
-type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+const theme = {
+  colors: {
+    primary: "rgb(35, 39, 47)",
+  },
 };
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: 'Sawarabi Mincho', sans-serif;
+    font-size: 0.625em;
+    font-weight: 400;
+    text-align: justify;
+    box-sizing: border-box;
+    letter-spacing: 1px;
+    overflow-x:hidden;
+  }
+  a{
+    text-decoration: none;
+    color: #111111;
+  }
+  input{
+          border: none;
+          outline: none;
+  }
+  button {
+    font: inherit;
+          cursor: pointer;
+          border: none;
+          outline: none;
+          appearance: none;
+  }
+  img {
+    max-width: 100%;
+    display: block;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+`;
